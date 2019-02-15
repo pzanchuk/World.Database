63c322c12e04a6a0fdb771e454394913d683fddb
Pavel Zanchuk <buzzik@yahoo.com>
2019-02-15 12:37:37 -0800

    Adjust RepeatCounter method and add test cases for each case

--- WordCounter.Tests/ModelTests/MyClassTests.cs
+++ WordCounter.Tests/ModelTests/MyClassTests.cs
@@ -19,5 +19,33 @@ namespace WordCounter.Tests
       MyClass newClass = new MyClass();
       Assert.AreEqual(0, newClass.RepeatCounter("I like sushi", "shoe"));
     }
+
+    [TestMethod]
+    public void RepeatCounter_CheckIfSingleMatchFound_True()
+    {
+      MyClass newClass = new MyClass();
+      Assert.AreEqual(1, newClass.RepeatCounter("Today was a nice day.", "was"));
+    }
+
+    [TestMethod]
+    public void RepeatCounter_CheckIfMultipleMatchesFound_True()
+    {
+      MyClass newClass = new MyClass();
+      Assert.AreEqual(2, newClass.RepeatCounter("I love my cat so much, and my cat loves me back.", "cat"));
+    }
+
+    [TestMethod]
+    public void RepeatCounter_CheckIfMultipleMatchesFoundAndCaseIgnored_True()
+    {
+      MyClass newClass = new MyClass();
+      Assert.AreEqual(2, newClass.RepeatCounter("Swimming whale is like swimming submarine ", "swimming"));
+    }
+
+    [TestMethod]
+    public void RepeatCounter_CheckIfMultipleMatchesFoundCaseAndPunctuationalMarksIgnored_True()
+    {
+      MyClass newClass = new MyClass();
+      Assert.AreEqual(3, newClass.RepeatCounter("Some flowers smell like ,some? grosserie in .some! store.", "Some"));
+    }
   }
 }
